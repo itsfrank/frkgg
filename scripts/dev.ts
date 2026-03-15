9e3f7b7a66066f95cdbf0b05be6d56cefada9181
itsfrank <frankobrien11@yahoo.ca>
2026-03-15 15:48:50 -0700

    begin caddy setup

---
+++ scripts/dev.ts
@@ -0,0 +1,25 @@
+import concurrently from 'concurrently';
+
+const commands = [
+    {
+        command: 'caddy run --config ./Caddyfile.dev',
+        name: 'caddy',
+        prefixColor: 'blue',
+    },
+    {
+        command: 'bun run --filter auth dev',
+        name: 'auth',
+        prefixColor: 'green',
+    },
+];
+
+const { result } = concurrently(commands, {
+    killOthers: ['failure', 'success'],
+    prefix: 'name',
+});
+
+try {
+    await result;
+} catch {
+    process.exitCode = 1;
+}
